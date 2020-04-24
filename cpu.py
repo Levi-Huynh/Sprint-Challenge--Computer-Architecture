957ccf8209f33658fcf9be6cc326b619cfd20b91
Levi-Huynh <Levi_huynh@yahoo.com>
2020-04-24 10:06:44 -0700

    JEQ, JMP, JNE, CMP added

--- cpu.py
+++ cpu.py
@@ -10,6 +10,10 @@ OP5 = 0b01000110  # POP
 OP6 = 0b01010000  # CALL
 OP7 = 0b00010001  # RET
 OP8 = 0b10100000  # ADD
+OP9 = 0b10100111  # CMP 167
+OP10 = 0b01010100  # jmp 84
+OP11 = 0b01010110  # jne 86
+OP12 = 0b01010101  # JEQ 85
 
 
 class CPU:
@@ -30,8 +34,13 @@ class CPU:
         self.branchtable[OP6] = self.handle_op6
         self.branchtable[OP7] = self.handle_op7
         self.branchtable[OP8] = self.handle_op8
-
+        self.branchtable[OP10] = self.handle_op10  # JMP
+        self.branchtable[OP11] = self.handle_op11  # JNE
+        self.branchtable[OP12] = self.handle_op12  # JEQ
         self.SP = 7
+        self.EFlag = 0
+        self.LFlag = 0
+        self.GFlag = 0
 
     def load(self):
         """Load a program into memory."""
@@ -61,8 +70,7 @@ class CPU:
                         converted = int("0b"+num, 2)  # converted to dec
 
                         self.memory[address] = converted
-                        # print(
-                        # f"dec: {converted}, bin_c: {bin_c} memory: {type(self.memory[address])} {type(converted)}")
+
                         address += 1
 
             except FileNotFoundError:
@@ -111,8 +119,7 @@ class CPU:
         self.pc += 3
 
     def handle_op4(self):
-        # PUSH register
-        # Push the value in the given register on the stack.
+        # PUSH the value in the given register on the stack.
         # Decrement the SP.
         # Copy the value in the given register to the address pointed to by SP.
         reg = self.ram_read(self.pc+1)
@@ -133,13 +140,13 @@ class CPU:
 
     def handle_op6(self):
         print("CALL!")
-        # The address of the instruction directly after CALL is PUSHED ONTO STACK
+        # The address of the instruction directly after CALL is PUSHED
         # decrement our SP to push instructions to stack
         self.reg[self.SP] -= 1
-        # This allows us to return to address where we left off when the subroutine finishes executing.
+        # allows us to return to address where we left off
         self.ram_write(self.pc+2, self.reg[self.SP])
         # The PC is set to the address stored in the given register.
-        # We jump to that location in RAM and execute the first instruction in the subroutine.
+        # We jump to that location in RAM and execute the first instruction
         reg = self.ram_read(self.pc+1)
         self.pc = self.reg[reg]
 
@@ -158,6 +165,36 @@ class CPU:
         self.reg[self.ram_read(self.pc+1)] = val
         self.pc += 3
 
+    def handle_op10(self):
+        print("JMP")
+        # Jump to the address stored in the given register.#??
+        reg = self.ram_read(self.pc+1)
+        # Set the PC to the address stored in the given register.
+        self.pc = self.reg[reg]
+        #self.pc += 2
+
+    def handle_op11(self):
+        print("JNE")
+        if self.EFlag == 0:
+            print("JNE true")
+            reg = self.memory[self.pc+1]
+            self.pc = self.reg[reg]
+        else:
+            # continue
+            self.pc += 2
+
+    def handle_op12(self):
+        print("JEQ")
+        if self.EFlag == 1:
+            print("JEQ True")
+            reg = self.memory[self.pc+1]
+            # update pc direct to go to address we want
+            self.pc = self.reg[reg]
+            #self.pc += 2
+        else:
+            self.pc += 2
+            # continue
+
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
@@ -165,7 +202,7 @@ class CPU:
         if op == 130:  # LOAD
             print("store!")
             self.reg[self.ram_read(self.pc+1)] = self.ram_read(self.pc+2)
-            # increment to next value in memory (after 1.opcode, 2. reg#, 3.value)
+            # increment to next value in memory after opcode
             self.pc += (num_operannds + 1)
 
         elif op == 162:
@@ -173,12 +210,24 @@ class CPU:
             # this needs to be self.reg
             regA_val = self.reg[self.ram_read(self.pc+1)]
             regB_val = self.reg[self.ram_read(self.pc+2)]
-
             mult_val = regA_val * regB_val
             # store the result in regA
             self.reg[self.ram_read(self.pc+1)] = mult_val
             # increment to next value in memory (after 1.opcode, 2. reg#, 3.value)
             self.pc += (num_operannds + 1)
+
+        elif op == 167:
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.EFlag = 1
+            if self.reg[reg_a] != self.reg[reg_b]:
+                self.EFlag = 0
+            if self.reg[reg_a] < self.reg[reg_b]:
+                self.LFlag = 1
+                #self.GFlag = 0
+            if self.reg[reg_a] > self.reg[reg_b]:
+                #self.LFlag = 0
+                self.GFlag = 1
+            self.pc += 3
 
         else:
             raise Exception("Unsupported ALU operation")
@@ -197,7 +246,6 @@ class CPU:
             if instruction == 0b00000001:
                 running = False
             elif instruction >> 7 == 1:
-                # if instruction >> 7 == 1:
                 # invoke self.alu
                 self.alu(instruction, self.ram_read(
                     self.pc+1), self.ram_read(self.pc+2))
